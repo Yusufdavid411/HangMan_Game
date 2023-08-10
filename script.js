b61c73f1ffe0394255997fdd21c5006090f55036
Yusufdavid411 <125559496+Yusufdavid411@users.noreply.github.com>
2023-08-10 18:13:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function Game(){
 
     ans = userInput.value;
 
-    if(ans.length == 1 && ans.includes('e')){
+    if(ans.length == 1 && ans.includes('y')){
         const won = document.querySelector('p')
         console.log(won.innerText = 'You Won!')
         const ansReturn = document.querySelector('h3')
-        console.log(ansReturn.innerText = 'Divine')
+        console.log(ansReturn.innerText = 'Timothy')
         won.style.color = 'green';
         ansReturn.style.borderStyle = 'solid';
         ansReturn.style.borderColor = 'white';
@@ -15,7 +15,7 @@ function Game(){
         const tryAgain = document.querySelector('p')
         console.log(tryAgain.innerText = 'Try Again?')
         const ansReturn = document.querySelector('h3')
-        console.log(ansReturn.innerText = 'Divin?')
+        console.log(ansReturn.innerText = 'Timoth?')
         tryAgain.style.color = 'red';
         ansReturn.style.borderStyle = 'solid';
         ansReturn.style.borderColor = 'red';
